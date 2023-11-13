2f2a356e548a75da787a59d6b497a3b7e3fce480
L-jy16 <89904583+uUZINN@users.noreply.github.com>
2023-11-13 10:25:06 +0900

    첫 셋팅

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,24 @@
+import Contact from '@/components/Contact'
+import Header from '@/components/Header'
+import Intro from '@/components/Intro'
+import Port from '@/components/Port'
+import Site from '@/components/Site'
+import Skill from '@/components/Skill'
+import Skip from '@/components/Skip'
+import React from 'react'
+
 export default function Home() {
   return (
-    <div>next.js</div>
+    <>
+      <Skip />
+      <Header />
+      <main id='main' role='main'>
+        <Intro />
+        <Skill />
+        <Site />
+        <Port />
+        <Contact />
+      </main>
+    </>
   )
 }
